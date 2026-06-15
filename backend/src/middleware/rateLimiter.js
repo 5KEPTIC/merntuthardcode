3b93503ffa18b3887a84781b122f29b5995e62ba
5KEPTIC <ekakshtyagi524@gmail.com>
2026-06-15 14:12:10 +0530

    no msg

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -2,16 +2,18 @@ import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
   try {
-    const { success } = await ratelimit.limit(req.ip);
+    const { success } = await ratelimit.limit("my-rate-limit");
 
     if (!success) {
-      return res.status(429).json({ message: "Too many requests" });
+      return res.status(429).json({
+        message: "Too many requests, please try again later",
+      });
     }
 
     next();
   } catch (error) {
     console.log("Rate limit error", error);
-    next();
+    next(error);
   }
 };
 
