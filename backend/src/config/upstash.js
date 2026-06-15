689de2125409f82502c689751fe90db1f3b25806
5KEPTIC <ekakshtyagi524@gmail.com>
2026-06-15 14:29:09 +0530

    new timeout limit

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const ratelimit = new Ratelimit({
     redis:Redis.fromEnv(),
-    limiter:Ratelimit.slidingWindow(5,"20s"),
+    limiter:Ratelimit.slidingWindow(100,"20s"),
 })
 
 export default ratelimit;
